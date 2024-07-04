1de621dffdc27331d389dd924e385f3586af62fb
Sérgio Izaque Pinheiro Carrozino <133893698+SergioIzaque@users.noreply.github.com>
2024-07-04 11:15:56 -0300

    Add files via upload

---
+++ listaduplaencad/listaduplamenteencadeada/applistadupla.java
@@ -0,0 +1,9 @@
+package listaduplamenteencadeada;
+
+public class applistadupla 
+{
+    public static void main(String[] args)
+    {
+        ListaDupla lista = new ListaDupla();    
+    }
+}
---
+++ listaduplaencad/listaduplamenteencadeada/arvorebin.java
@@ -0,0 +1,26 @@
+public class ArvoreBin {    
+    private class EsqDir
+    {
+    private int numero;
+    private EsqDir dir = null;
+    private EsqDir esq = null;
+    }
+
+    public class EsqDir(int novoNumero, EsqDir dirDir, EsqDir esqEsq)
+    {
+        numero = novoNumero;
+        dir 
+    }
+
+    public boolean vazio()
+    {
+        if(dir == null && esq == null)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
